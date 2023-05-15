91faa27cee0031af44867079c9343baf05197beb
Lucas Nguyen <quandh1810@sendme.kr>
2023-05-16 00:27:44 +0700

    api url

--- src/utils/trpc-provider.tsx
+++ src/utils/trpc-provider.tsx
@@ -17,7 +17,7 @@ export const TrpcProvider: React.FC<{ children: React.ReactNode }> = ({
   );
 
   const url = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/apis/trpc`
     : "http://localhost:3000/apis/trpc/";
 
   const [trpcClient] = useState(() =>
